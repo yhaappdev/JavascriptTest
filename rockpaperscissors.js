98fbc4795c65f719075cff57cbd0a66f709c3fb9
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-04-04 09:36:23 +0800

    edit UI settings

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,7 +3,19 @@ let computerScore = 0;
 //let playerSelection = getPlayerChoice();
 //let computerSelection = getComputerChoice();
 //console.log(playRound(playerSelection, computerSelection));
-console.log(game());
+const result = document.querySelector('#result');
+const content = document.createElement('div');
+content.classList.add('content');
+//content.textContent = game();
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorBtn = document.getElementById('scissorBtn');
+const restartBtn = document.getElementById('restartBtn');
+
+rockBtn.addEventListener('click', () => alert("Rock Clicked"));
+paperBtn.addEventListener('click', () => alert("Paper Clicked"));
+scissorBtn.addEventListener('click', () => alert("Scissors Clicked"));
+restartBtn.addEventListener('click', () => alert('Restart Clicked'));
 
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
@@ -12,13 +24,16 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
+    
     const playerChoice = prompt('Please input your choice. [Rock, Paper, Scissors]').toLowerCase();
+    
     return playerChoice;
 }
 
 function playRound(){
     let log = '';
-    let playerSelection = getPlayerChoice();
+
+    //let playerSelection = button;
     let computerSelection = getComputerChoice();
     //add more cases later
     if((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) 
@@ -43,7 +58,7 @@ function playRound(){
 
 function game(){
 
-    for(let i = 0; i < 5; i++){
+    //for(let i = 0; i < 5; i++){
         
         let log = playRound();
         if( log === 'Win'){
@@ -52,11 +67,15 @@ function game(){
         else if(log === 'Lose'){
             computerScore ++;
         }
+        /*
         else if(playerScore == 5 || computerScore == 5){
             break;
         }
-        console.log(i);
-    }
-    console.log('Player Score: ' +playerScore);
-    console.log('Computer Score: ' + computerScore);
+        */
+       content.textContent = 'Player Score: ' +playerScore;
+       content.textContent = 'Computer Score: ' +computerScore;
+    //}
+    //console.log('Player Score: ' +playerScore);
+    //console.log('Computer Score: ' + computerScore);
 }
+
